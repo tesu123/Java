34fbfd53187430afe9746a4dccd8226d12e16cdd
tesu123 <abhijit.rabidas.mca@gmail.com>
2025-04-30 12:44:16 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,49 +1,63 @@
 import java.util.Scanner;
 
-// Node class
+// Node class to represent each element in the linked list
 class Node {
     int data;
     Node next;
 
-    Node(int data) {
+    public Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
 
-// Main class
-public class LinkedList {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// Linked list class
+class LinkedList {
+    Node head;
 
-        Node head = null;
-        Node tail = null;
-
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
-
-        // Input and build the linked list
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter element " + (i + 1) + ": ");
-            int value = sc.nextInt();
-
-            Node newNode = new Node(value);
-
-            if (head == null) {
-                head = newNode;  // First node becomes head
-                tail = newNode;  // Tail also points to the same
-            } else {
-                tail.next = newNode;  // Link last node to new node
-                tail = newNode;       // Move tail to new node
-            }
+    // Method to add a new node at the end
+    public void append(int data) {
+        Node newNode = new Node(data);
+        
+        if (head == null) {
+            head = newNode;
+            return;
         }
 
-        // Display the linked list
-        System.out.print("Linked List: ");
         Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " ");
+        while (current.next != null) {
             current = current.next;
         }
+        current.next = newNode;
+    }
+
+    // Method to display the linked list
+    public void display() {
+        Node current = head;
+        System.out.print("Linked List: ");
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            int data = scanner.nextInt();
+            list.append(data);
+        }
+
+        list.display();
+        scanner.close();
     }
 }
